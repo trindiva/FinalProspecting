5d0c31af7af89a582b7dc15d4f7b34087d0701c5
Ivan Trindev <trindiva@uw.edu>
2019-10-30 10:24:51 -0700

    Added in code files

---
+++ FinalProspecting.py
@@ -0,0 +1,165 @@
+import pandas as pd
+from pandas import ExcelWriter
+from pandas import ExcelFile
+import os
+from tkinter import *
+from tkinter import filedialog
+
+# Get the data frame of raw data, get info, and turn it into a list
+def info_data_email(raw_data_path):
+    # get the raw data and read it
+    path = raw_data_path
+
+    df = pd.read_excel(path, dtype=str)
+
+    # Leave only column with "….." dividers, remove other two
+    df = df.drop(columns = ["Col", "….."])
+
+    # Leave only the last column (has info and "…..") and make elements all strings
+    df = df["Col.1"].astype(str)
+
+    # Turn data into a list 
+    info_list = df.tolist()
+
+    return (info_list)
+
+# Get info for each row and create list of final info
+def get_rows_email(info_list):
+    info = info_list
+    final_list = []
+
+    # Get index of all "….."
+    indices = [i for i, x in enumerate(info) if x == "….."]
+
+    # get the individual row info from the info
+    # get the first row (fencepost)
+    
+    # get index of first instance of "….."
+    first_index = indices[0]
+    
+    # use index to get info for first row from info
+    row_info = info[:first_index]
+
+    # clean the row of unnecessary info
+    clean_row = clean_rows_email(row_info)
+
+    # add row_info to final_list
+    final_list.extend(clean_row)
+    
+    # get all the rest of the rows 
+    count = 2
+    while count < len(indices) - 1:
+        print(count)
+        # get consecutive occurences of "….."
+        bottom_index = indices[count]
+        top_index = indices[count + 1]
+
+        # use indices to get row info from the cleaned_list
+        row_info = info[bottom_index + 1:top_index]
+        clean_row = clean_rows_email(row_info)
+
+        # add row_info to final_list
+        final_list.extend(clean_row)
+        count += 3
+        print(clean_row)
+
+    return (final_list)
+
+# Clean the row info
+def clean_rows_email(row_info):
+    clean_row= []
+    
+    # Remove all the elements between the title and the company name
+    if "B" in row_info:
+        row_info.remove("B")
+
+    if "D" in row_info:
+        row_info.remove("D")
+
+    if "HQ" in row_info:
+        row_info.remove("HQ")
+
+    if "-" in row_info:
+        row_info.remove("-")
+
+    if "-" in row_info:
+        row_info.remove("-")
+
+    # Add in the first three elements (name, title, company name)
+    first_three = row_info[:3]
+    clean_row.extend(first_three)
+
+    # Remove the country
+    if ", " in row_info[3]:
+        row_info.remove(row_info[3])
+
+    # Check for industry; put "PLACEHOLDER!" if there is not one
+    if "43" in row_info[3]:
+        clean_row.append("")
+    else:
+        clean_row.append(row_info[3])
+
+    # Find the email and add it to clean_row
+    email_indices = [i for i, s in enumerate(row_info) if '@' in s]
+    email_index = email_indices[0]
+    email = row_info[email_index]
+    email = email.split(' ', 1)[0]
+    clean_row.append(email)
+
+    # Find the phone number and add it to clean row (direct if possible, HQ if not)
+    phone_indices = [i for i, s in enumerate(row_info) if '(Direct)' in s]
+    if len(phone_indices) == 0:
+        phone_indices = [i for i, s in enumerate(row_info) if '(HQ)' in s]
+    phone_number_index = phone_indices[0]
+    phone_number = row_info[phone_number_index]
+    phone_number = phone_number[:14]
+    clean_row.append(phone_number)
+
+    return(clean_row)
+
+# Write final info into excel sheet
+def organize_info_email(raw_path, final_path):
+    info_list = info_data_email(raw_path)
+    final_list = get_rows_email(info_list)
+
+    # elements are in order: name, title, company, industry, email, phone
+    # get evert sixth element for each type of info
+    name = final_list[0::6]
+    title = final_list[1::6]
+    company = final_list[2::6]
+    industry = final_list[3::6]
+    email = final_list[4::6]
+    phone = final_list[5::6]
+
+    final_df = pd.DataFrame(list(zip(name, title, company, industry, email, phone)), columns = ["Full Name", "Title", "Company", "Industry", "Email", "Phone Number"])
+    final_df.to_excel(final_path, sheet_name='Sheet1', index=False)
+
+# Get the path for raw data, name/directory for export file
+def get_paths():
+    # initialize ability to browse files
+    root = Tk()
+    root.withdraw()
+    
+    # user chooses raw data file
+    print("Select raw data file:")
+    root.rawdata =  filedialog.askopenfilename(initialdir = "/",title = "Select Raw Data File")
+    print()
+
+    # user chooses how to name final, exported file
+    print("How would you like to name your finished file?")
+    final_file_name = input()
+    print()
+
+    print("Select export file:")
+    root.finalfile =  filedialog.askdirectory(initialdir = "/", title = "Select Export Location")
+    print(root.finalfile)
+
+    # get the final path
+    final_file_name += ".xlsx"
+    final_path = root.finalfile + "/" + final_file_name
+
+    # pass paths to organize_info_email (this function will do the rest)
+    organize_info_email(root.rawdata, final_path)
+
+
+get_paths()
